e744c9ad432da0879389f575d04b0efeb6f7cf95
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-07-16 13:02:56 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Sum from "./Sum";
+
+function App(){
+    
+}
--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+// In a parent component render a div that shows the sum result of two numbers of your choice , the result should be saved in a state and finally share the result to a paragraph in a child component.
+
+import React from "react";
+
+function EyesOnMe(){
+    const focusHandler = () =>{
+        console.log("Good!")
+    }
+
+    const blurHandler = () =>{
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <div>
+            <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
